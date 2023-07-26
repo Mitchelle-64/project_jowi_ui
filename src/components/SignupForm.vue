a76aa84a6e872336f52e15ce5c37bd09f1aa0524
Mitchelle-64 <51542475+Mitchelle-64@users.noreply.github.com>
2023-07-26 14:18:04 +0300

    users can now login. Simple styling has been added to the forms

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,46 +1,52 @@
 <template>
-    <div class="signup">
+    <div class="flex items-center justify-center h-screen bg-dark-blue">
+      <div class="w-150 p-6 rounded-lg bg-white shadow-lg">
+        <h2 class="text-3xl text-center font-semibold text-dark-blue mb-6">Sign Up</h2>
         <form class="form flex flex-col gap-4" @submit.prevent="signupShipper">
-            <div class="inputDiv">
-                <label for="firstName"> first name: </label>
-                <input type="text" placeholder="enter first name" v-model="formData.firstName"/>
-            </div>
-            <div class="inputDiv">
-                <label for="lastName"> last name: </label>
-                <input type="text" placeholder="enter your last name" v-model="formData.lastName" />
-            </div>
-            <div class="inputDiv">
-                <label for="phoneNumber"> Phone number: </label>
-                <input type="number" placeholder="enter your phone number" v-model="formData.phoneNumber" />
-            </div>
-            <div class="inputDiv">
-                <label for="email"> email: </label>
-                <input type="email" placeholder="enter your email" v-model="formData.email" />
-            </div>
-            <div class="inputDiv">
-                <label for="password"> Password: </label>
-                <input type="password" placeholder="enter your password" v-model="formData.password"/>
-            </div>
-            <div class="inputDiv">
-                <label for="confirm_password"> confirm password: </label>
-                <input type="password" placeholder="confirm password" v-model="formData.confirm_password"/>
-            </div>
-            <div class="inputDiv">
-                <button type="submit"> Sign up </button>
-            </div>
-            
+          <div class="inputDiv">
+            <label for="firstName" class="text-dark-blue">First Name:</label>
+            <input type="text" placeholder="Enter your first name" v-model="formData.firstName" class="input" />
+          </div>
+          <div class="inputDiv">
+            <label for="lastName" class="text-dark-blue">Last Name:</label>
+            <input type="text" placeholder="Enter your last name" v-model="formData.lastName" class="input" />
+          </div>
+          <div class="inputDiv">
+            <label for="phoneNumber" class="text-dark-blue">Phone Number:</label>
+            <input type="text" placeholder="Enter your phone number" v-model="formData.phoneNumber" class="input" />
+          </div>
+          <div class="inputDiv">
+            <label for="email" class="text-dark-blue">Email:</label>
+            <input type="email" placeholder="Enter your email" v-model="formData.email" class="input" />
+          </div>
+          <div class="inputDiv">
+            <label for="password" class="text-dark-blue">Password:</label>
+            <input type="password" placeholder="Enter your password" v-model="formData.password" class="input" />
+          </div>
+          <div class="inputDiv">
+            <label for="confirmPassword" class="text-dark-blue">Confirm Password:</label>
+            <input type="password" placeholder="Confirm your password" v-model="formData.confirm_password" class="input" />
+          </div>
+          <div class="inputDiv">
+            <button type="submit" class="btn rounded-full bg-light-blue px-6 py-1 text-white text-xl">
+              Sign Up
+            </button>
+          </div>
         </form>
-        <div v-if="formData">
-            <h1> you have submitted the following data </h1>
-            <pre> {{ formData }}</pre>
-        </div>
+        <!-- <div v-if="formData">
+          <h1 class="text-center mt-4">You have submitted the following data:</h1>
+          <pre class="text-center">{{ formData }}</pre>
+        </div> -->
+      </div>
     </div>
-    
 </template>
+  
+  
 
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
+import { useRouter } from 'vue-router';
 
 const formData = ref({
   firstName: '',
@@ -50,6 +56,9 @@ const formData = ref({
   password: '',
   confirm_password:'',
 });
+
+const router = useRouter();
+
 
 // Function to handle shipper signup
 const signupShipper = async () => {
@@ -68,6 +77,8 @@ const signupShipper = async () => {
     };
 
     console.log('shipper signed up successfully')
+    router.push({name: 'login'});
+
 
     return response;
   } catch (error) {
@@ -82,13 +93,16 @@ const signupShipper = async () => {
 @import 'tailwindcss/components';
 @import 'tailwindcss/utilities';
 
-.form {
-    @apply box-border w-1/4 border-solid border-2 border-dark-blue rounded-lg p-6 justify-center content-center
-
+.inputDiv {
+  @apply border-solid;
 }
+
 .input {
-     
+  @apply px-4 py-2 border rounded-md border-gray-300 focus:outline-none focus:border-light-blue;
 }
 
+h2 {
+    font-family: 'Comfortaa', cursive;
+  }
 
 </style>
